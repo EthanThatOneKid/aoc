e21babeb13609788ab78c15b50e6dbac19a12650
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2022-12-05 01:23:06 -0800

    Finish day 5 part 2

--- 2022/05/solutions/ts/main.ts
+++ 2022/05/solutions/ts/main.ts
@@ -9,66 +9,58 @@ import { parse } from "https://deno.land/std@0.167.0/flags/mod.ts";
 const flags = parse(Deno.args);
 const isPart2 = flags.part2 ?? flags.p2 ?? flags["2"];
 
-function part1(input: string[]) {
+function solution(input: string[]) {
   const cratesConfig = input.slice(0, input.indexOf("") - 1);
   const movesConfig = input.slice(input.indexOf("") + 1);
 
-  const crates = cratesConfig.reduce(
+  const cols = cratesConfig.reduce(
     (
-      stacks: [
-        string,
-        string,
-        string,
-        string,
-        string,
-        string,
-        string,
-        string,
-        string,
-      ],
-      row,
+      stacks: string[],
+      {
+        1: one,
+        5: two,
+        9: three,
+        13: four,
+        17: five,
+        21: six,
+        25: seven,
+        29: eight,
+        33: nine,
+      },
     ) => {
-      const {
-        1: i,
-        5: ii,
-        9: iii,
-        13: iv,
-        17: v,
-        21: vi,
-        25: vii,
-        29: viii,
-        33: ix,
-      } = row;
-      [i, ii, iii, iv, v, vi, vii, viii, ix].forEach((c, i) => {
+      [one, two, three, four, five, six, seven, eight, nine].forEach((c, i) => {
         if (c !== " ") {
           stacks[i] = c + stacks[i];
         }
       });
       return stacks;
     },
-    ["", "", "", "", "", "", "", "", ""],
+    Array.from({ length: 9 }, () => "" as string),
   );
 
   movesConfig.forEach((move) => {
     const { 1: amt, 3: from, 5: to } = move.split(" ").map(Number);
     const fromIdx = from - 1;
     const toIdx = to - 1;
-    const stuff = crates[fromIdx]
-      .slice(crates[fromIdx].length - amt).split("").reverse().join("");
-    crates[toIdx] += stuff;
-    crates[fromIdx] = crates[fromIdx].slice(
+    const stuff = cols[fromIdx].slice(cols[fromIdx].length - amt);
+
+    if (isPart2) {
+      cols[toIdx] += stuff;
+    } else {
+      cols[toIdx] += stuff.split("").reverse().join("");
+    }
+
+    cols[fromIdx] = cols[fromIdx].slice(
       0,
-      crates[fromIdx].length - amt,
+      cols[fromIdx].length - amt,
     );
   });
 
-  const result = crates.map((c) => c[c.length - 1]).join("");
+  const result = cols.map((c) => c[c.length - 1]).join("");
 
   console.log({ result });
 }
 
-function part2(input: string[]) {}
-
 const input = Deno.readTextFileSync("input.txt").split("\r\n");
 
-isPart2 ? part2(input) : part1(input);
+solution(input);
