d92d6bc5accca06bcc1e6526988b24806cec32c6
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-12-09 12:12:51 -0800

    day 9 part 2

--- 2024/09/solution.ts
+++ 2024/09/solution.ts
@@ -1,15 +1,28 @@
 // deno run --allow-read 2024/09/solution.ts
 if (import.meta.main) {
   const input = await Deno.readTextFile(
-    new URL(import.meta.resolve("./my-input")),
+    new URL(import.meta.resolve("./input")),
   );
   console.log("Part 1", part1(input));
+  console.log("Part 2", part2(input));
 }
 
 function part1(input: string): number {
   const disk = parseDisk(input);
-  const movedDisk = moveFileBlocks(disk);
-  return checksum(movedDisk);
+  const moved = moveFileBlocks(disk);
+  return checksum(moved);
+}
+
+function part2(input: string): number {
+  const disk = parseDisk(input);
+  const moved = moveWholeFiles(disk);
+  return checksum(moved);
+}
+
+function _renderDisk(disk: Disk): string {
+  return disk
+    .map((block) => block === -1 ? "." : block)
+    .join("");
 }
 
 function checksum(disk: Disk): number {
@@ -23,6 +36,70 @@ function checksum(disk: Disk): number {
   }
 
   return sum;
+}
+
+function moveWholeFiles(disk: Disk): Disk {
+  const result = disk.slice();
+  for (const [fileStart, fileEnd] of byDecreasingFileID(result)) {
+    for (const [freeStart, freeEnd] of freeRanges(result)) {
+      if (
+        freeStart >= fileStart ||
+        freeEnd - freeStart < fileEnd - fileStart
+      ) {
+        continue;
+      }
+
+      moveWholeFile(result, fileStart, fileEnd, freeStart);
+    }
+  }
+
+  return result;
+}
+
+function moveWholeFile(
+  disk: Disk,
+  start: number,
+  end: number,
+  destinationStart: number,
+) {
+  for (let i = start; i <= end; i++) {
+    swap(disk, i, destinationStart + i - start);
+  }
+}
+
+function byDecreasingFileID(disk: Disk): Array<[number, number]> {
+  const files: Array<[number, number]> = [];
+  for (let i = disk.length - 1; i >= 0; i--) {
+    if (disk[i] === -1) {
+      continue;
+    }
+
+    const id = disk[i];
+    const start = disk
+      .findLastIndex((block, j) => j < i && block !== id) +
+      1;
+    files.push([start, i]);
+    i = start;
+  }
+
+  return files.toSorted((a, b) => disk[b[0]] - disk[a[0]]);
+}
+
+function* freeRanges(disk: Disk): Generator<[number, number]> {
+  let end = 0;
+  while (true) {
+    const start = disk.indexOf(-1, end);
+    if (start === -1) {
+      break;
+    }
+
+    end = disk.findIndex((block, i) => i > start && block !== -1);
+    if (end === -1) {
+      end = disk.length;
+    }
+
+    yield [start, end - 1];
+  }
 }
 
 function moveFileBlocks(disk: Disk): Disk {
