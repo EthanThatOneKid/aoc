ea310b7792a7287d95ccc3ce7db008d14773f214
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-12-09 23:16:33 -0800

    day 10 part 2

--- 2024/10/solution.ts
+++ 2024/10/solution.ts
@@ -1,27 +1,42 @@
 // deno run --allow-read 2024/10/solution.ts
 if (import.meta.main) {
   const input = await Deno.readTextFile(
-    new URL(import.meta.resolve("./my-input")),
+    new URL(import.meta.resolve("./input")),
   );
-  console.log("Part 1", part1(input));
+  console.log("Part 1", part1(input)); // 36
+  console.log("Part 2", part2(input)); // 81
 }
 
 function part1(input: string): number {
   const topology = parseTopology(input);
 
   let sum = 0;
-  for (let row = 0; row < topology.length; row++) {
-    for (let column = 0; column < topology[row].length; column++) {
-      const trailhead = topology[row][column] === 0;
-      if (!trailhead) {
-        continue;
-      }
-
-      sum += score(topology, row, column).size;
-    }
+  for (const [row, column] of walkTrailheads(topology)) {
+    sum += score(topology, row, column).size;
   }
 
   return sum;
+}
+
+function part2(input: string): number {
+  const topology = parseTopology(input);
+
+  let sum = 0;
+  for (const [row, column] of walkTrailheads(topology)) {
+    sum += rating(topology, row, column);
+  }
+
+  return sum;
+}
+
+function* walkTrailheads(topology: Topology): Generator<[number, number]> {
+  for (let row = 0; row < topology.length; row++) {
+    for (let column = 0; column < topology[row].length; column++) {
+      if (topology[row][column] === 0) {
+        yield [row, column];
+      }
+    }
+  }
 }
 
 function score(topology: Topology, row: number, column: number): Set<number> {
@@ -56,6 +71,38 @@ function score(topology: Topology, row: number, column: number): Set<number> {
   }
 
   return visited;
+}
+
+function rating(topology: Topology, row: number, column: number): number {
+  const height = topology[row][column];
+  if (height === 9) {
+    return 1;
+  }
+
+  let value = 0;
+  for (let dx = -1; dx <= 1; dx++) {
+    for (let dy = -1; dy <= 1; dy++) {
+      if (Math.abs(dx) === Math.abs(dy)) {
+        continue;
+      }
+
+      const newRow = row + dx;
+      const newColumn = column + dy;
+      if (
+        newRow < 0 ||
+        newRow >= topology.length ||
+        newColumn < 0 ||
+        newColumn >= topology[newRow].length ||
+        topology[newRow][newColumn] !== height + 1
+      ) {
+        continue;
+      }
+
+      value += rating(topology, newRow, newColumn);
+    }
+  }
+
+  return value;
 }
 
 function linearIndex(columns: number, row: number, column: number): number {
