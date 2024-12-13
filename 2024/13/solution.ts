1f7c0b716deca44a748ae7c02b3bf4d0c64779f2
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-12-13 00:41:09 -0800

    day 13 part 2

--- 2024/13/solution.ts
+++ 2024/13/solution.ts
@@ -6,6 +6,7 @@ if (import.meta.main) {
     new URL(import.meta.resolve("./input")),
   );
   console.log("Part 1", part1(input)); // 480
+  console.log("Part 2", part2(input)); // 416082282239
 }
 
 function part1(input: string): number {
@@ -13,9 +14,29 @@ function part1(input: string): number {
   return machines.reduce((sum, machine) => sum + cheapestPrize(machine), 0);
 }
 
+function part2(input: string): number {
+  const machines = parseMachines(input);
+  return machines.reduce(
+    (sum, machine) => sum + cheapestPrize(fixMachine(machine)),
+    0,
+  );
+}
+
+function fixMachine(
+  { buttonA, buttonB, prize: [prize0, prize1] }: Machine,
+  fix = 10_000_000_000_000,
+): Machine {
+  return {
+    buttonA,
+    buttonB,
+    prize: [prize0 + fix, prize1 + fix],
+  };
+}
+
 function cheapestPrize(machine: Machine): number {
-  const { buttonA: [x0, x1], buttonB: [y0, y1], prize: [z0, z1] } = machine;
-  const x = array([[x0, y0], [x1, y1]]).solve(array([[z0], [z1]]));
+  const { buttonA: [aX, aY], buttonB: [bX, bY], prize: [prizeX, prizeY] } =
+    machine;
+  const x = array([[aX, bX], [aY, bY]]).solve(array([[prizeX], [prizeY]]));
   const a = x.get(0, 0);
   const b = x.get(1, 0);
   if (!isWhole(a) || !isWhole(b)) {
@@ -25,7 +46,7 @@ function cheapestPrize(machine: Machine): number {
   return 3 * Math.round(a) + Math.round(b);
 }
 
-function isWhole(n: number, negligeable = 0.0000001): boolean {
+function isWhole(n: number, negligeable = 0.0000000000001): boolean {
   const diff = n - Math.floor(n);
   if (diff > negligeable && diff < 1 - negligeable) {
     return false;
