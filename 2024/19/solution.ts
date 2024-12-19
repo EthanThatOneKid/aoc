17abb4ae2a1a410c1e78b843671673ddd38ca2f2
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2024-12-19 11:18:48 -0800

    day 19 part 2

--- 2024/19/solution.ts
+++ 2024/19/solution.ts
@@ -4,45 +4,51 @@ if (import.meta.main) {
     new URL(import.meta.resolve("./input")),
   );
   console.log("Part 1", part1(input)); // 6
+  console.log("Part 2", part2(input)); // 16
 }
 
 function part1(input: string): number {
   const { designs, patterns } = parseConfig(input);
   const possibleDesigns = designs
-    .filter((design) => possible(patterns, design));
+    .filter((design) => possible(patterns, design) > 0);
   return possibleDesigns.length;
+}
+
+function part2(input: string): number {
+  const { designs, patterns } = parseConfig(input);
+  return designs.reduce((sum, design) => sum + possible(patterns, design), 0);
 }
 
 function possible(
   patterns: string[],
   design: string,
-  memo: Set<string> = new Set(),
-): boolean {
+  memo: Map<string, number> = new Map(),
+): number {
   if (memo.has(design)) {
-    return false;
+    return memo.get(design)!;
   }
 
   if (design.length === 0) {
-    return true;
+    return 1;
   }
 
+  let count = 0;
   const potentialPatterns = patterns
     .filter((pattern) => design.startsWith(pattern));
   for (const pattern of potentialPatterns) {
-    if (possible(patterns, design.slice(pattern.length), memo)) {
-      return true;
-    }
+    count += possible(patterns, design.slice(pattern.length), memo);
   }
 
-  memo.add(design);
-  return false;
+  memo.set(design, count);
+  return count;
 }
 
 function parseConfig(input: string): Config {
   const [patternsString, designsString] = input.split("\n\n");
-  const patterns = patternsString.split(", ");
-  const designs = designsString.split("\n");
-  return { patterns, designs };
+  return {
+    patterns: patternsString.split(", "),
+    designs: designsString.split("\n"),
+  };
 }
 
 interface Config {
