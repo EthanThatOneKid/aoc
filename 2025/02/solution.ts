aa07140f332e7486a012daf784fc4b13ce173116
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2025-12-01 23:02:45 -0800

    Solved 2025 day 2 part 1

---
+++ 2025/02/solution.ts
@@ -0,0 +1,62 @@
+// deno --allow-read 2025/02/solution.ts
+if (import.meta.main) {
+    const input = await Deno.readTextFile(
+        new URL(import.meta.resolve("./input")),
+    );
+    const _exampleInput =
+        `11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124`;
+
+    console.log(
+        "Part 1",
+        part1(input),
+    );
+    console.log("Part 2", part2(input));
+}
+
+function part1(input: string): number {
+    const ranges = parseInput(input);
+    const invalidIds = ranges
+        .map((range) => getInvalidIds(range))
+        .flat()
+        .reduce((sum, invalidId) => sum + invalidId, 0);
+    return invalidIds;
+}
+
+function part2(input: string): number {
+    return 0;
+}
+
+function getInvalidIds(range: Range): number[] {
+    const invalidIds = [];
+    for (let i = range.firstId; i <= range.lastId; i++) {
+        if (validateId(i)) {
+            continue;
+        }
+
+        invalidIds.push(i);
+    }
+
+    return invalidIds;
+}
+
+function validateId(id: number) {
+    const idString = id.toString();
+    if (idString.length % 2 === 1) {
+        return true;
+    }
+
+    return idString.slice(0, idString.length / 2) !==
+        idString.slice(idString.length / 2);
+}
+
+interface Range {
+    firstId: number;
+    lastId: number;
+}
+
+function parseInput(input: string): Range[] {
+    return input.split(",").map((line) => {
+        const [firstId, lastId] = line.split("-").map((id) => parseInt(id));
+        return { firstId, lastId };
+    });
+}
